36ee2fd34a95d73bd4751239f8056a48a0a9add1
Rob <betoarpi@gmail.com>
2019-09-30 13:41:19 -0500

    Animación completa en el hamburger icon mobile nav

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,11 @@
+// eslint-disable-next-line camelcase
 const $navigation__mobile = document.querySelector('.navigation__mobile');
 $navigation__mobile.addEventListener('click', event => {
-  event.preventDefault;
-  $navigation__mobile.classList.add('active');
+  event.preventDefault();
+  if ($navigation__mobile.classList.value === 'navigation__mobile active') {
+    $navigation__mobile.classList.add('inactive');
+    setTimeout(() => $navigation__mobile.classList.remove('active', 'inactive'), 1000)
+  } else {
+    $navigation__mobile.classList.add('active');
+  }
 });
