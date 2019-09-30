ca40ae28ef06cd3e7658f1577a1ba3aea54e3c40
Rob <betoarpi@gmail.com>
2019-09-30 15:28:44 -0500

    Transisión de mobile nav lista

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,20 @@
 // eslint-disable-next-line camelcase
+const $main = document.querySelector('.main');
 const $navigation__mobile = document.querySelector('.navigation__mobile');
+const $navigation__website = document.querySelector('.navigation__website');
+
 $navigation__mobile.addEventListener('click', event => {
   event.preventDefault();
   if ($navigation__mobile.classList.value === 'navigation__mobile active') {
     $navigation__mobile.classList.add('inactive');
-    setTimeout(() => $navigation__mobile.classList.remove('active', 'inactive'), 1000)
+    setTimeout(() => {
+      $navigation__mobile.classList.remove('active', 'inactive')
+      $navigation__website.classList.remove('active');
+      $main.classList.remove('mobile-active');
+    }, 600)
   } else {
     $navigation__mobile.classList.add('active');
+    $navigation__website.classList.add('active');
+    $main.classList.add('mobile-active');
   }
 });
