28ad75420959f5bb748dbcda7ce6f68c50e310dd
jcmexdev <carlos.garcia.esquivel@live.com.mx>
2019-10-17 18:36:07 -0500

    Se integro la pantalla de login sin responsive

--- src/routes/App.js
+++ src/routes/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from '../containers/Home';
 import Salon from '../containers/Salon';
 import Layout from '../containers/Layout';
+import Login from '../containers/Login';
 
 const App = () => (
   <BrowserRouter>
@@ -10,6 +11,7 @@ const App = () => (
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/salon' component={Salon} />
+        <Route exact path='/login' component={Login} />
       </Switch>
     </Layout>
   </BrowserRouter>
