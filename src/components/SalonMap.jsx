19e5ca082ceb29b55542d2ca1459eb8b26e5f8fd
Rob <betoarpi@gmail.com>
2019-10-17 21:32:32 -0500

    Se creó el container para el salón y sus componentes.

---
+++ src/components/SalonMap.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../assets/styles/sass/components/Salon.scss';
+import '../assets/styles/sass/components/Gallery.scss';
+
+const SalonMap = () => {
+  return (
+    <div className='salon-details__map'>
+      <img src='/src/img/map-placeholder.png' alt='map' />
+    </div>
+  );
+};
+
+export default SalonMap;
