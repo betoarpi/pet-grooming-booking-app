9816077814c351836232e83ba62c4974d2a62727
Rob <betoarpi@gmail.com>
2019-10-28 22:37:16 -0600

    Funcionalidad de menú movil

--- src/js/index.js
+++ src/js/index.js
@@ -3,12 +3,12 @@ const $main = document.querySelector('.main');
 const $navigation__mobile = document.querySelector('.navigation__mobile');
 const $navigation__website = document.querySelector('.navigation__website');
 
-$navigation__mobile.addEventListener('click', event => {
+$navigation__mobile.addEventListener('click', (event) => {
   event.preventDefault();
   if ($navigation__mobile.classList.value === 'navigation__mobile active') {
     $navigation__mobile.classList.add('inactive');
     setTimeout(() => {
-      $navigation__mobile.classList.remove('active', 'inactive')
+      $navigation__mobile.classList.remove('active', 'inactive');
       $navigation__website.classList.remove('active');
       $main.classList.remove('mobile-active');
     }, 600);
