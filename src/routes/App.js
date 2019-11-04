dc30cef5fbfe4373cf82325cd7a22303fe5b64f2
Rob <betoarpi@gmail.com>
2019-11-03 20:26:36 -0600

    Se añadió history para manejar el scroll top al enlazar a las páginas interiores después de usar el smooth scroll hacia un elemento.

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,20 +1,31 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Router, Switch, Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import Home from '../containers/Home';
 import Salon from '../containers/Salon';
 import NotFound from '../containers/NotFound';
 import Layout from '../containers/Layout';
 
-const App = () => (
-  <BrowserRouter>
-    <Layout>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/salon' component={Salon} />
-        <Route component={NotFound} />
-      </Switch>
-    </Layout>
-  </BrowserRouter>
-);
+const history = createBrowserHistory();
+
+history.listen((location, action) => {
+  window.scrollTo(0, 0);
+});
+
+class App extends Component {
+  render() {
+    return (
+      <Router history={history}>
+        <Layout>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/salon' component={Salon} />
+            <Route component={NotFound} />
+          </Switch>
+        </Layout>
+      </Router>
+    );
+  }
+};
 
 export default App;
