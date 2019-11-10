a4c3d2e37b796084730815f81f7e69eb3f9c5c41
Rob Arroyo <betoarpi@gmail.com>
2019-11-09 20:10:05 -0600

    Modal de Disponibilidad con estilos solo para desktop

--- src/components/ReservationModal.jsx
+++ src/components/ReservationModal.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Modal from './Modal';
-import '../assets/styles/sass/components/ReservationModal.scss';
+import '../assets/styles/sass/components/bookingModal.scss';
 
 const DayAvailability = (props) => {
     const {modalOpen, onClose} = props;
@@ -11,106 +11,87 @@ const DayAvailability = (props) => {
 
                 <div className='day-availability__calendar'>
                     <header>
-                        <h4 className='day-availability__date'>Jueves 31 de Octubre del 2019</h4>
+                        <h5 className='day-availability__date'>Jueves 31 de Octubre del 2019</h5>
                         <div className='prev'><i className='fas fa-chevron-left'></i></div>
                         <div className='next'><i className='fas fa-chevron-right'></i></div>
                     </header>
 
-                    <div className='day-availability__hour'>
+                    <div className='day-availability__hour not-available'>
                         <div className='day-availability__time'>
-                            <time datetime='9:00'>9:00</time> A.M.
+                            <time dateTime='9:00'>9:00</time> A.M.
                         </div>
-                        <div className='day-availability__reservation'>
-                            something
+                        <div className='day-availability__booking'>
+                            Horario Reservado
                         </div>
                     </div>
                     <div className='day-availability__hour'>
                         <div className='day-availability__time'>
-                            <time datetime='10:00'>10:00</time> A.M.
+                            <time dateTime='10:00'>10:00</time> A.M.
                         </div>
-                        <div className='day-availability__reservation'>
-                            something
+                        <div className='day-availability__booking'> </div>
+                    </div>
+                    <div className='day-availability__hour'>
+                        <div className='day-availability__time'>
+                            <time dateTime='11:00'>11:00</time> A.M.
+                        </div>
+                        <div className='day-availability__booking'> </div>
+                    </div>
+                    <div className='day-availability__hour'>
+                        <div className='day-availability__time'>
+                            <time dateTime='12:00'>12:00</time> P.M.
+                        </div>
+                        <div className='day-availability__booking'> </div>
+                    </div>
+                    <div className='day-availability__hour booked'>
+                        <div className='day-availability__time'>
+                            <time dateTime='13:00'>13:00</time> P.M.
+                        </div>
+                        <div className='day-availability__booking'>
+                            Reservado para Morita
+                            <span>Ver Detalles</span>
                         </div>
                     </div>
                     <div className='day-availability__hour'>
                         <div className='day-availability__time'>
-                            <time datetime='11:00'>11:00</time> A.M.
+                            <time dateTime='14:00'>14:00</time> P.M.
                         </div>
-                        <div className='day-availability__reservation'>
-                            something
-                        </div>
+                        <div className='day-availability__booking'> </div>
                     </div>
                     <div className='day-availability__hour'>
                         <div className='day-availability__time'>
-                            <time datetime='12:00'>12:00</time> P.M.
+                            <time dateTime='15:00'>15:00</time> P.M.
                         </div>
-                        <div className='day-availability__reservation'>
-                            something
-                        </div>
+                        <div className='day-availability__booking'> </div>
                     </div>
                     <div className='day-availability__hour'>
                         <div className='day-availability__time'>
-                            <time datetime='13:00'>13:00</time> P.M.
+                            <time dateTime='16:00'>16:00</time> P.M.
                         </div>
-                        <div className='day-availability__reservation'>
-                            something
-                        </div>
+                        <div className='day-availability__booking'> </div>
                     </div>
                     <div className='day-availability__hour'>
                         <div className='day-availability__time'>
-                            <time datetime='14:00'>14:00</time> P.M.
+                            <time dateTime='17:00'>17:00</time> P.M.
                         </div>
-                        <div className='day-availability__reservation'>
-                            something
-                        </div>
+                        <div className='day-availability__booking'> </div>
                     </div>
                     <div className='day-availability__hour'>
                         <div className='day-availability__time'>
-                            <time datetime='15:00'>15:00</time> P.M.
+                            <time dateTime='18:00'>18:00</time> P.M.
                         </div>
-                        <div className='day-availability__reservation'>
-                            something
-                        </div>
+                        <div className='day-availability__booking'> </div>
                     </div>
                     <div className='day-availability__hour'>
                         <div className='day-availability__time'>
-                            <time datetime='16:00'>16:00</time> P.M.
+                            <time dateTime='19:00'>19:00</time> P.M.
                         </div>
-                        <div className='day-availability__reservation'>
-                            something
-                        </div>
+                        <div className='day-availability__booking'> </div>
                     </div>
                     <div className='day-availability__hour'>
                         <div className='day-availability__time'>
-                            <time datetime='17:00'>17:00</time> P.M.
+                            <time dateTime='20:00'>20:00</time> P.M.
                         </div>
-                        <div className='day-availability__reservation'>
-                            something
-                        </div>
-                    </div>
-                    <div className='day-availability__hour'>
-                        <div className='day-availability__time'>
-                            <time datetime='18:00'>18:00</time> P.M.
-                        </div>
-                        <div className='day-availability__reservation'>
-                            something
-                        </div>
-                    </div>
-                    <div className='day-availability__hour'>
-                        <div className='day-availability__time'>
-                            <time datetime='19:00'>19:00</time> P.M.
-                        </div>
-                        <div className='day-availability__reservation'>
-                            something
-                        </div>
-                    </div>
-                    <div className='day-availability__hour'>
-                        <div className='day-availability__time'>
-                            <time datetime='20:00'>20:00</time> P.M.
-                        </div>
-                        <div className='day-availability__reservation'>
-                            something
-                        </div>
+                        <div className='day-availability__booking'> </div>
                     </div>
                 </div>
             </div>
