255bf4975d329c7a8c0540b0db957c61d386f8ce
Rob <betoarpi@gmail.com>
2019-11-10 19:15:46 -0600

    Modal de Confirmación de Servicio con estilos responsivos

---
+++ src/components/BookingConfirmation.jsx
@@ -0,0 +1,120 @@
+import React from 'react';
+
+const BookingConfirmation = () => {
+  return (
+    <div className='booking-confirmation active'>
+      <h4 className='booking-confirmation__title'>Confirma tu Servicio</h4>
+
+      <div className='booking-confirmation__container'>
+        <header>
+          <i className='far fa-calendar-alt'> </i>
+          <h2 className='booking-confirmation__date'>
+            <span className='day'>Jueves</span>
+            <span className='date'>31 de Septiembre</span>
+          </h2>
+          <time dateTime='14:00'>14:00 P.M.</time>
+        </header>
+
+        <form action='' className='booking-confirmation__form'>
+          <div className='booking-confirmation__pet-type'>
+            <h6>Selecciona a tu tipo de mascota:</h6>
+            <i className='fas fa-dog'> </i>
+            <select name='pet-type' id='pet-type'>
+              <option value='dog'>
+                Perro
+              </option>
+              <option value='cat'>
+                Gato
+              </option>
+            </select>
+          </div>
+
+          <div className='booking-confirmation__pet-info'>
+            <label htmlFor='pet-name'>
+              <span>Nombre de tu mascota</span>
+              <input name='pet-name' type='text' />
+            </label>
+
+            <label htmlFor='pet-breed'>
+              <span>Raza</span>
+              <select name='pet-breed' id='pet-breed'>
+                <option value='big-breed'>
+                  Raza Grande
+                </option>
+                <option value='medium-breed'>
+                  Raza Mediana
+                </option>
+                <option value='small-breed'>
+                  Raza Chica
+                </option>
+              </select>
+            </label>
+          </div>
+
+          <div className='booking-confirmation__services'>
+            <h6>Selecciona los Servicios de Grooming para tu mascota:</h6>
+            <div className='booking-confirmation__services__checkboxes'>
+              <div className='col'>
+                <label htmlFor='bath'>
+                  <span>
+                    <i className='fas fa-check-square'> </i>
+                    <input type='checkbox' name='bath' id='bath' />
+                  </span>
+                  Baño
+                </label>
+                <label htmlFor='haircut'>
+                  <span>
+                    <i className='far fa-square'> </i>
+                    <input type='checkbox' name='haircut' id='haircut' />
+                  </span>
+                  Corte
+                </label>
+                <label htmlFor='teeth-brush'>
+                  <span>
+                    <i className='far fa-square'> </i>
+                    <input type='checkbox' name='teeth-brush' id='teeth-brush' />
+                  </span>
+                  Limpieza Dental
+                </label>
+              </div>
+              <div className='col'>
+                <label htmlFor='flea-control'>
+                  <span>
+                    <i className='far fa-square'> </i>
+                    <input type='checkbox' name='flea-control' id='flea-control' />
+                  </span>
+                  Anti-Pulgas
+                </label>
+                <label htmlFor='special-haircut'>
+                  <span>
+                    <i className='far fa-square'> </i>
+                    <input type='checkbox' name='special-haircut' id='special-haircut' />
+                  </span>
+                  Corte para Concurso
+                </label>
+              </div>
+            </div>
+          </div>
+
+          <div className='booking-confirmation__notes'>
+            <label htmlFor='notes'>
+              <h6>Notas para el groomer:</h6>
+              <textarea name='notes' id='notes' cols='30' rows='10' placeholder='Escribe alguna nota para la persona que atenderá tu mascota' />
+            </label>
+          </div>
+
+          <div className='booking-confirmation__submit'>
+            <span className='price'>
+              Costo: $250.00 MXN
+            </span>
+            <button type='submit' className='btn btn--dark'>
+              Agendar Servicio
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default BookingConfirmation;
--- src/components/BookingModal.jsx
+++ src/components/BookingModal.jsx
@@ -1,102 +1,18 @@
 import React from 'react';
 import Modal from './Modal';
-import '../assets/styles/sass/components/BookingModal.scss';
+import DayAvailability from './DayAvailability';
+import BookingConfirmation from './BookingConfirmation';
+import '../assets/styles/sass/components/DayAvailability.scss';
+import '../assets/styles/sass/components/BookingConfirmation.scss';
 
-const DayAvailability = (props) => {
-    const { modalOpen, onClose } = props;
-    return (
-        <Modal modalOpen={modalOpen} onClose={onClose}>
-            <div className='day-availability'>
-                <h2><i className='far fa-calendar-alt'></i> Disponibilidad</h2>
+const BookingModal = (props) => {
+  const { modalOpen, onClose } = props;
+  return (
+    <Modal modalOpen={modalOpen} onClose={onClose}>
+      <DayAvailability />
+      <BookingConfirmation />
+    </Modal>
+  );
+};
 
-                <div className='day-availability__calendar'>
-                    <header>
-                        <h5 className='day-availability__date'>Jueves 31 de Octubre del 2019</h5>
-                        <div className='prev'><i className='fas fa-chevron-left'></i></div>
-                        <div className='next'><i className='fas fa-chevron-right'></i></div>
-                    </header>
-
-                    <div className='day-availability__hour not-available'>
-                        <div className='day-availability__time'>
-                            <time dateTime='9:00'>9:00</time> A.M.
-                    </div>
-                        <div className='day-availability__booking'>
-                            Horario Reservado
-                    </div>
-                    </div>
-                    <div className='day-availability__hour'>
-                        <div className='day-availability__time'>
-                            <time dateTime='10:00'>10:00</time> A.M.
-                    </div>
-                        <div className='day-availability__booking'> </div>
-                    </div>
-                    <div className='day-availability__hour'>
-                        <div className='day-availability__time'>
-                            <time dateTime='11:00'>11:00</time> A.M.
-                    </div>
-                        <div className='day-availability__booking'> </div>
-                    </div>
-                    <div className='day-availability__hour'>
-                        <div className='day-availability__time'>
-                            <time dateTime='12:00'>12:00</time> P.M.
-                    </div>
-                        <div className='day-availability__booking'> </div>
-                    </div>
-                    <div className='day-availability__hour booked'>
-                        <div className='day-availability__time'>
-                            <time dateTime='13:00'>13:00</time> P.M.
-                    </div>
-                        <div className='day-availability__booking'>
-                            Reservado para Morita
-                      <span>Ver Detalles</span>
-                        </div>
-                    </div>
-                    <div className='day-availability__hour'>
-                        <div className='day-availability__time'>
-                            <time dateTime='14:00'>14:00</time> P.M.
-                    </div>
-                        <div className='day-availability__booking'> </div>
-                    </div>
-                    <div className='day-availability__hour'>
-                        <div className='day-availability__time'>
-                            <time dateTime='15:00'>15:00</time> P.M.
-                    </div>
-                        <div className='day-availability__booking'> </div>
-                    </div>
-                    <div className='day-availability__hour'>
-                        <div className='day-availability__time'>
-                            <time dateTime='16:00'>16:00</time> P.M.
-                    </div>
-                        <div className='day-availability__booking'> </div>
-                    </div>
-                    <div className='day-availability__hour'>
-                        <div className='day-availability__time'>
-                            <time dateTime='17:00'>17:00</time> P.M.
-                    </div>
-                        <div className='day-availability__booking'> </div>
-                    </div>
-                    <div className='day-availability__hour'>
-                        <div className='day-availability__time'>
-                            <time dateTime='18:00'>18:00</time> P.M.
-                    </div>
-                        <div className='day-availability__booking'> </div>
-                    </div>
-                    <div className='day-availability__hour'>
-                        <div className='day-availability__time'>
-                            <time dateTime='19:00'>19:00</time> P.M.
-                    </div>
-                        <div className='day-availability__booking'> </div>
-                    </div>
-                    <div className='day-availability__hour'>
-                        <div className='day-availability__time'>
-                            <time dateTime='20:00'>20:00</time> P.M.
-                    </div>
-                        <div className='day-availability__booking'> </div>
-                    </div>
-                </div>
-            </div>
-        </Modal>
-    );
-}
-
-export default DayAvailability;
+export default BookingModal;
