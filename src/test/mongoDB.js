7d30bddd69d7c78e2c5b1837f0b2b9483c91a15d
jcmexdev <carlos.garcia.esquivel@live.com.mx>
2019-11-14 20:48:01 -0600

    database config was moved to backen

--- src/test/mongoDB.js
+++
@@ -1,7 +0,0 @@
-const MongoConnection = require('../lib/mongo');
-
-const connection = new MongoConnection();
-connection
-  .connect()
-  .then((db) => process.exit())
-  .catch((error) => process.exit(error));
