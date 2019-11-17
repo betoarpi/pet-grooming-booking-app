34aa35e38b175eb76fe31e3e624ed5c26dc51685
Rob <betoarpi@gmail.com>
2019-11-16 21:38:31 -0600

    Eventos de manejo de input y submit para el registro

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/styles/sass/views/_login.scss';
 
@@ -12,7 +13,7 @@ const Login = () => {
     setFormValues({
       ...fomrValues,
       [event.target.name]: event.target.value,
-    })
+    });
   };
 
   const handleSubmit = event => {
@@ -21,52 +22,53 @@ const Login = () => {
   };
 
   return (
-  <section className='login'>
-    <div className='login__container'>
-      <form className='login__left' onSubmit={handleSubmit}>
-        <h3 className='login__title'>
-          Inicia Sesión
-        </h3>
-        <div className='login__input-group'>
-          <label className='login__label'>Nombre de Usuario</label>
-          <input type='email' name='email' onChange={handleInput} />
-        </div>
-        <div className='login__input-group'>
-          <label className='login__label'>Contraseña</label>
-          <input type='password' name='password' onChange={handleInput} />
-        </div>
-        <div className='login__actions'>
-          <div className='login__rememberme'>
-            <input type='checkbox' />
-            <span>Recuérdame</span>
+    <section className='login'>
+      <div className='login__container'>
+        <form className='login__left' onSubmit={handleSubmit}>
+          <h3 className='login__title'>
+            Inicia Sesión
+          </h3>
+          <div className='login__input-group'>
+            <label className='login__label'>Nombre de Usuario</label>
+            <input type='email' name='email' onChange={handleInput} />
           </div>
-          <span>Olvidaste tu contraseña?</span>
-        </div>
-        <div className='login__social-buttons'>
-          <div className='login__facebook-button'>
-            <i className='fab fa-facebook-f facebook__icon'></i>
-            <span className='facebook__title'>facebook</span>
+          <div className='login__input-group'>
+            <label className='login__label'>Contraseña</label>
+            <input type='password' name='password' onChange={handleInput} />
           </div>
-          <div className='login__google-button'>
-            <i className='fab fa-google google__icon'></i>
-            <span className='google__title'>google</span>
+          <div className='login__actions'>
+            <div className='login__rememberme'>
+              <input type='checkbox' />
+              <span>Recuérdame</span>
+            </div>
+            <span>Olvidaste tu contraseña?</span>
           </div>
+          <div className='login__social-buttons'>
+            <div className='login__facebook-button'>
+              <i className='fab fa-facebook-f facebook__icon'> </i>
+              <span className='facebook__title'>facebook</span>
+            </div>
+            <div className='login__google-button'>
+              <i className='fab fa-google google__icon'> </i>
+              <span className='google__title'>google</span>
+            </div>
+          </div>
+          <div className='login__submit'>
+            <button type='submit'>Continuar</button>
+          </div>
+          <div className='login__register'>
+            <Link to='/register'>¿No tienes cuenta? Regístrate</Link>
+          </div>
+        </form>
+        <div className='login__right'>
+          <h3 className='login__welcome'>
+            Bienvenido
+          </h3>
+          <div className='login__divider' />
         </div>
-        <div className="login__submit">
-          <button type='submit'>Continuar</button>
-        </div>
-        <div className="login__register">
-          <Link to='/register'>¿No tienes cuenta? Regístrate</Link>
-        </div>
-      </form>
-      <div className='login__right'>
-        <h3 className="login__welcome">
-          Bienvenido
-        </h3>
-        <div className="login__divider" />
       </div>
-    </div>
-  </section>
-)};
+    </section>
+  );
+};
 
 export default Login;
--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,51 +1,70 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/styles/sass/views/_register.scss';
 
-const Register = () => (
-  <section className='register'>
-    <div className='register__container'>
-      <div className='register__left'>
-        <h3 className='register__title'>
-          Regístrate
-        </h3>
-        <div className='register__input-group'>
-          <label className='register__label'>Nombre de Usuario</label>
-          <input type='text' name='username' />
-        </div>
-        <div className='register__input-group'>
-          <label className='register__label'>Correo electrónico</label>
-          <input type='email' name='email' />
-        </div>
-        <div className='register__input-group'>
-          <label className='register__label'>Contraseña</label>
-          <input type='password' name='password' />
-        </div>
-        <div className='register__social-buttons'>
-          <div className='register__facebook-button'>
-            <i className='fab fa-facebook-f facebook__icon'></i>
-            <span className='facebook__title'>facebook</span>
+const Register = () => {
+  const [formValues, setFormValues] = useState({
+    username: ' ',
+    email: ' ',
+    password: ' ',
+  });
+  const handleInput = event => {
+    setFormValues({
+      ...formValues,
+      [event.target.name]: event.target.value,
+    });
+  };
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(formValues);
+  };
+
+  return (
+    <section className='register'>
+      <div className='register__container'>
+        <form className='register__left' onSubmit={handleSubmit}>
+          <h3 className='register__title'>
+            Regístrate
+          </h3>
+          <div className='register__input-group'>
+            <label className='register__label'>Nombre de Usuario</label>
+            <input type='text' name='username' onChange={handleInput} />
           </div>
-          <div className='register__google-button'>
-            <i className='fab fa-google google__icon'></i>
-            <span className='google__title'>google</span>
+          <div className='register__input-group'>
+            <label className='register__label'>Correo electrónico</label>
+            <input type='email' name='email' onChange={handleInput} />
           </div>
-        </div>
-        <div className='register__submit'>
-          <button type='submit'>Continuar</button>
-        </div>
-        <div className="register__login">
-          <Link to='/login'>¿Ya tienes una cuenta? Inicia sesión</Link>
+          <div className='register__input-group'>
+            <label className='register__label'>Contraseña</label>
+            <input type='password' name='password' onChange={handleInput} />
+          </div>
+          <div className='register__social-buttons'>
+            <div className='register__facebook-button'>
+              <i className='fab fa-facebook-f facebook__icon'> </i>
+              <span className='facebook__title'>facebook</span>
+            </div>
+            <div className='register__google-button'>
+              <i className='fab fa-google google__icon'> </i>
+              <span className='google__title'>google</span>
+            </div>
+          </div>
+          <div className='register__submit'>
+            <button type='submit'>Continuar</button>
+          </div>
+          <div className='register__login'>
+            <Link to='/login'>¿Ya tienes una cuenta? Inicia sesión</Link>
+          </div>
+        </form>
+        <div className='register__right'>
+          <h3 className='register__welcome'>
+            Bienvenido
+          </h3>
+          <div className='register__divider' />
         </div>
       </div>
-      <div className='register__right'>
-        <h3 className="register__welcome">
-          Bienvenido
-        </h3>
-        <div className="register__divider" />
-      </div>
-    </div>
-  </section>
-);
+    </section>
+  )
+};
 
 export default Register;
