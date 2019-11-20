1de74d7726d72ba5d2a0c83825bf3288c4f3086a
jcmexdev <carlos.garcia.esquivel@live.com.mx>
2019-11-19 21:52:44 -0600

    crud and testing for clients resource without auth strategies

--- backend/utils/schemas/client.js
+++ backend/utils/schemas/client.js
@@ -1,31 +1,45 @@
 const joi = require('@hapi/joi');
 
 const idStructure = /^[0-9a-fA-F]{24}$/;
-
 const clientIdSchema = joi.string().regex(idStructure);
-const userIdSchema = joi.string().regex(idStructure);
+const fullNameSchema = joi.string().max(200);
+const phoneSchema = joi.string().max(15);
+const addressSchema = joi.string().max(300);
+const postalCodeSchema = joi
+  .number()
+  .integer()
+  .positive();
+const avatarSchema = joi.string().max(500);
+const emailSchema = joi.string().email();
 const usernameSchema = joi.string().max(80);
 const passwordSchema = joi
   .string()
   .min(8)
   .max(80);
-const favoritesSchema = joi.array().items(joi.string().regex(idStructure));
+const isAdminSchema = joi.boolean();
 
 const createClientSchema = {
+  fullName: fullNameSchema,
+  phone: phoneSchema,
+  address: addressSchema,
+  postalCode: postalCodeSchema,
+  avatar: avatarSchema,
+  email: emailSchema.required(),
   username: usernameSchema.required(),
   password: passwordSchema.required(),
-  user_id: userIdSchema.required()
+  isAdmin: isAdminSchema.required()
 };
 
 const updateClientSchema = {
+  fullName: fullNameSchema,
+  phone: phoneSchema,
+  address: addressSchema,
+  postalCode: postalCodeSchema,
+  avatar: avatarSchema,
+  email: emailSchema,
   username: usernameSchema,
   password: passwordSchema,
-  favorites: favoritesSchema,
-  user_id: userIdSchema
+  isAdmin: isAdminSchema
 };
 
-module.exports = {
-  clientIdSchema,
-  createClientSchema,
-  updateClientSchema
-};
+module.exports = { clientIdSchema, createClientSchema, updateClientSchema };
